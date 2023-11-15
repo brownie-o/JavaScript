cb1939077219fc77ea39e29b8d05042707f33ab9
brownie-o <amyooxx1024@gmail.com>
2023-11-15 15:36:35 +0800

    匯率bot

--- 231115/index.js
+++ 231115/index.js
@@ -5,6 +5,17 @@ import linebot from 'linebot'
 import fe from './commands/fe.js'
 import be from './commands/be.js'
 import anime from './commands/anime.js'
+import { scheduleJob } from 'node-schedule'
+import * as usdtwd from './data/usdtwd.js'
+
+// https://crontab.guru/every-day
+scheduleJob('0 0 * * *', () => {
+  // 每天0點更新
+  usdtwd.update()
+})
+// 剛開啟機器人的時候要更新
+// ctrl + 點擊 就會跑到該function
+usdtwd.update()
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -15,7 +26,10 @@ const bot = linebot({
 // bot.on('message') => 代表收到訊息時
 // https://www.npmjs.com/package/linebot
 bot.on('message', event => {
-  console.log(event)
+  if (process.env.DEBUG === 'true') {
+    console.log(event)
+  }
+
   if (event.message.type === 'text') {
     if (event.message.text === '前端') {
       fe(event)
@@ -24,6 +38,8 @@ bot.on('message', event => {
       // .startsWith('') 訊息是以''開頭
     } else if (event.message.text.startsWith('動畫')) {
       anime(event)
+    } else if (event.message.text === '匯率') {
+      event.reply(usdtwd.exrate.toString())
     }
   }
 })
