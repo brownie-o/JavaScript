ebe8035fd667a5c16328189e57d5ac29dbcfc7e7
brownie-o <amyooxx1024@gmail.com>
2023-11-15 14:16:16 +0800

    be 後端bot

--- 231115/templates/be.js
+++ 231115/templates/be.js
@@ -1,28 +1,26 @@
-export default {
-  type: 'bubble',
-  hero: {
-    type: 'image',
-    url: 'https://wdaweb.github.io/index_assets/img/node.jpg',
-    size: 'full',
-    aspectRatio: '20:13',
-    aspectMode: 'cover',
-    action: {
-      type: 'uri',
-      uri: 'http://linecorp.com/'
+// export default: 把JSON變成js檔
+export default () => {
+  // 包成function 每次呼叫都可以return一個乾淨的模板 就不會被前一份改的模板影響
+  return {
+    type: 'bubble',
+    hero: {
+      type: 'image',
+      url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+      size: 'full',
+      aspectRatio: '20:13',
+      aspectMode: 'cover'
     },
-    align: 'center',
-    position: 'relative'
-  },
-  body: {
-    type: 'box',
-    layout: 'vertical',
-    contents: [
-      {
-        type: 'text',
-        text: 'Brown Cafe',
-        weight: 'bold',
-        size: 'xl'
-      }
-    ]
+    body: {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          text: 'Brown Cafe',
+          weight: 'bold',
+          size: 'xl'
+        }
+      ]
+    }
   }
 }
