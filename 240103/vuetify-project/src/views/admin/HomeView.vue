694e3e07b6ba4d04f0352bd4ca380a2314c7713d
brownie-o <amyooxx1024@gmail.com>
2024-01-25 09:01:09 +0800

    v-bind color change

--- 240103/vuetify-project/src/views/admin/HomeView.vue
+++ 240103/vuetify-project/src/views/admin/HomeView.vue
@@ -2,5 +2,18 @@
 VContainer.h-100
   VRow.h-100(align='center')
     VCol(cols="12")
-      h1.text-center 請選擇管理項目
+      h1.text-center.text 請選擇管理項目
+      VBtn(@click="color = 'red'") 紅色
+      VBtn(@click="color = 'blue'") 藍色
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const color = ref('red')
+</script>
+
+<style scoped lang="sass">
+.text
+  color: v-bind(color)
+</style>
